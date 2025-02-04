46124b922ffed266942edb664746cac9795ec574
Freddy Song <fredsong99@gmail.com>
2025-02-03 22:19:52 -0800

    chore: init commit

---
+++ main.py
@@ -0,0 +1,13 @@
+"""
+    CS170 Project 1 - Eight Puzzle
+    Freddy Song
+    
+    Requirements:
+        - Uniform Cost Search
+        - A* with the Misplaced Tile heuristic
+        - A* with the Manhattan Distance heuristic
+        
+        This is all my work, excluding template or example code.
+        
+    Purpose: Entry point for running the program with user input or default puzzle.
+"""
