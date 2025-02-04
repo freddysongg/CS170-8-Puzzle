ad7899c88ef64134953eeefb8f7dbec8e15f0863
Freddy Song <fredsong99@gmail.com>
2025-02-03 23:54:27 -0800

    feat: metric data, add vars

--- node.py
+++ node.py
@@ -10,16 +10,33 @@ class Node:
         self.action = action
         self.path_cost = path_cost
         self.heuristic_cost = heuristic_cost
-        self.nodes_expanded = 0
+        self.expanded_nodes = 0
         self.max_queue_size = 0
 
-    def update_metrics(self, nodes_expanded, max_queue_size):
-        self.nodes_expanded = nodes_expanded
+    def update_metrics(self, expanded_nodes, max_queue_size):
+        self.expanded_nodes = expanded_nodes
         self.max_queue_size = max_queue_size
 
     def total_cost(self):
         # f(n) = g(n) + h(n)
         return self.path_cost + self.heuristic_cost
+
+    def get_soln(self):
+        path = []
+        current = self
+        # Tuple made of (state, action, path_cost, heuristic_cost)
+        while current:
+            path.append(
+                (
+                    current.state,
+                    current.action,
+                    current.path_cost,
+                    current.heuristic_cost,
+                )
+            )
+            current = current.parent
+        # Reverse to get the path from start to goal
+        return path[::-1]
 
     def __lt__(self, other):
         return self.total_cost() < other.total_cost()
